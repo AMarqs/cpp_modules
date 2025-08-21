34d269434cc0f33cdc736f46c80b4b9bc9ebc483
albmarqu <albmarqu@student.42malaga.com>
2025-08-21 20:41:36 +0200

    cpp01 ex02

---
+++ cpp01/ex02/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main()
+{
+	std::string str = "HI THIS IS BRAIN";
+	std::string *stringPTR = &str;
+	std::string &stringREF = str;
+
+	std::cout << "MEMORY ADDRESS" << std::endl;
+	std::cout << "String: " << &str << std::endl;
+	std::cout << "Pointer: " << stringPTR << std::endl;
+	std::cout << "Reference: " << &stringREF << std::endl;
+
+	std::cout << std::endl;
+
+	std::cout << "VALUE" << std::endl;
+	std::cout << "String: " << str << std::endl;
+	std::cout << "Pointer: " << *stringPTR << std::endl;
+	std::cout << "Reference: " << stringREF << std::endl;
+}
