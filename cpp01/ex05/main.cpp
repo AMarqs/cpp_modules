b6641fc1a7c19f0d0611e86dcdcfdc73091fcf1f
albmarqu <albmarqu@student.42malaga.com>
2025-08-22 20:47:15 +0200

    cpp01 ex05

--- cpp01/ex05/main.cpp
+++ cpp01/ex05/main.cpp
@@ -1,7 +1,24 @@
+#include "Harl.hpp"
 #include <iostream>
 
 int main()
 {
-	
+	Harl harl;
+
+	std::cout << "Debug level: ";
+	harl.complain("DEBUG");
+
+	std::cout << "Info level: ";
+	harl.complain("INFO");
+
+	std::cout << "Warning level: ";
+	harl.complain("WARNING");
+
+	std::cout << "Error level: ";
+	harl.complain("ERROR");
+
+	std::cout << "Worng level: ";
+	harl.complain("wefgwtrbws");
+
 	return 0;
 }
