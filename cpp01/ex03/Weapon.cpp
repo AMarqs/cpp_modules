e61afc35f23bdeeb05f2ad62672613ff26917215
albmarqu <albmarqu@student.42malaga.com>
2025-08-22 19:28:30 +0200

    cpp01 ex03

---
+++ cpp01/ex03/Weapon.cpp
@@ -0,0 +1,12 @@
+#include "Weapon.hpp"
+
+Weapon::Weapon() {}
+Weapon::Weapon(std::string type) : _type(type) {}
+Weapon::~Weapon() {}
+
+const std::string	Weapon::getType() const { return _type; }
+
+void	Weapon::setType( std::string new_type )
+{
+	_type = new_type;
+}
