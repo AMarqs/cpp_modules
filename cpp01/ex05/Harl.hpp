45cc35706d331e5016cdf14594296f289c2da128
amarqs <amarqs@email.com>
2025-09-11 20:37:50 +0200

    cpp01 ex05 print modification

--- cpp01/ex05/Harl.hpp
+++ cpp01/ex05/Harl.hpp
@@ -7,15 +7,16 @@ class Harl
 {
 	private:
 
-		void	debug( void );
-		void	info( void );
-		void	warning( void );
-		void	error( void );
+		void	debug(void);
+		void	info(void);
+		void	warning(void);
+		void	error(void);
 
 	public:
 
 		Harl();
 		~Harl();
+
 		void	complain( std::string level );
 };
 
--- cpp01/ex05/main.cpp
+++ cpp01/ex05/main.cpp
@@ -5,19 +5,19 @@ int main()
 {
 	Harl harl;
 
-	std::cout << "DEBUG LEVEL -> ";
+	std::cout << "--- DEBUG LEVEL ---" << std::endl;
 	harl.complain("DEBUG");
 
-	std::cout << std::endl << "INFO LEVEL -> ";
+	std::cout << std::endl << "--- INFO LEVEL ---" << std::endl;
 	harl.complain("INFO");
 
-	std::cout << std::endl << "WARNING LEVEL -> ";
+	std::cout << std::endl << "--- WARNING LEVEL ---" << std::endl;
 	harl.complain("WARNING");
 
-	std::cout << std::endl << "ERROR LEVEL -> ";
+	std::cout << std::endl << "--- ERROR LEVEL ---" << std::endl;
 	harl.complain("ERROR");
 
-	std::cout << std::endl << "WRONG LEVEL -> ";
+	std::cout << std::endl << "--- WRONG LEVEL ---" << std::endl;
 	harl.complain("wefgwtrbws");
 
 	return 0;
